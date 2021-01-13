da8d38c2bddefffba8a9a74d3cd17e6475f6ec0e
Powi <powi@powi.fr>
2021-01-13 03:20:20 +0100

    Update config.js

--- docs/content/.vuepress/config.js
+++ docs/content/.vuepress/config.js
@@ -66,5 +66,11 @@ module.exports = {
   plugins: [
     '@vuepress/plugin-back-to-top',
     ["vuepress-plugin-clean-urls", { normalSuffix: "/" }],
+    '@vuepress/last-updated',
+      {
+        dateOptions:{
+          hour12: false
+        }
+      },
   ],
 }
